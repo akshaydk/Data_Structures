6053eb30b02ed1a1587eeb83b010b42dc939d15f
akshaydk <dhupamakshay.kumar@gmail.com>
2015-08-25 08:25:10 -0700

    Update Bubble_Sort.py

--- Sorting/Bubble_Sort.py
+++ Sorting/Bubble_Sort.py
@@ -1,17 +1,17 @@
 __author__ = 'Akshay Kumar'
 
-def bubble_sort(a):
-    for i in range(len(a)):
+def bubble_sort(a):                             //bubble_sort function declaration for sorting the data in ascending order
+    for i in range(len(a)):     
         for k in range(len(a)-1,i,-1):
-            if(a[k]<a[k-1]):
+            if(a[k]<a[k-1]):                    // changing the logical operator will change the order form asec/decs
                 swap(a,k,k-1)
 
-def swap(a,x,y):
+def swap(a,x,y):                                // swap funtion that swap to data values in given list
     temp=a[x]
     a[x]=a[y]
     a[y]=temp
 
 if __name__=='__main__':
-    a=[19,0,25,1,6,34,64,123,5,665]
-    bubble_sort(a)
-    print(a)
+    a=[19,0,25,1,6,34,64,123,5,665]     // creating a list in python 
+    bubble_sort(a)                      // calling the bubble sort function
+    print(a)                            // printing
