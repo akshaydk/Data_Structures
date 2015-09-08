c43ce4a7b0f66e7c36dc058f8fb0cc5c7858c2dd
akshaydk <dhupamakshay.kumar@gmail.com>
2015-09-08 05:35:14 -0700

    new programs

---
+++ Binary Trees/delete_tree.py
@@ -0,0 +1,66 @@
+__author__ = 'Akshay Kumar'
+
+class binary_tree_node:
+
+    def __init__(self):
+        self.data=None
+        self.left=None
+        self.right=None
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self,data):
+        self.data=data
+
+    def left_node(self):
+        return self.left
+
+    def right_node(self):
+        return self.right
+
+class binary_tree:
+    def __init__(self):
+         self.root=None
+
+    def create_root(self,data):
+         new_node=binary_tree_node()
+         new_node.set_data(data)
+         self.root=new_node
+         return self.root
+
+    def insert_element(self,node,data):
+        if(node.left_node()==None):
+            new_node=binary_tree_node()
+            new_node.set_data(data)
+            node.left=new_node
+            return
+
+        elif(node.right_node()==None):
+            new_node=binary_tree_node()
+            new_node.set_data(data)
+            node.right=new_node
+            return
+        else:
+            self.insert_element(node.left_node(),data)
+
+
+    def delete_tree(self,node):
+        if node is None:
+            return
+        else:
+
+            self.delete_tree(node.left_node())
+            self.delete_tree(node.right_node())
+            print 'deleting node',node.get_data()
+            del node
+
+if __name__=='__main__':
+    new_tree=binary_tree()
+    node = new_tree.create_root(1)
+    new_tree.insert_element(node,2)
+    new_tree.insert_element(node,3)
+    new_tree.insert_element(node,4)
+    new_tree.insert_element(node,5)
+    new_tree.delete_tree(node)
+
---
+++ Binary Trees/queue_lib.py
@@ -0,0 +1,79 @@
+__author__ = 'Akshay'
+
+class Node():
+
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self,data):
+        self.data = data
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self,next):
+        self.next = next
+
+
+class my_queue():
+
+    def __init__(self):
+        self.head= None
+
+    def enqueue(self,data):
+        new_node=Node()
+        if(self.head==None):
+            new_node.set_data(data)
+            self.head=new_node
+            new_node.set_next(None)
+        else:
+            new_node.set_data(data)
+            current=self.head
+            while current.get_next()!=None:
+                current=current.get_next()
+            current.set_next(new_node)
+            new_node.set_next(None)
+
+    def queue_length(self):
+        current=self.head
+        count=0
+        while current!=None:
+            count=count+1
+            current=current.get_next()
+        return count
+
+    def is_empty(self):
+        current=self.head
+        count=0
+        while current!=None:
+            count=count+1
+            current=current.get_next()
+        if count==0:
+            return True
+
+
+    def dequeue(self):
+        length=self.queue_length()
+        if length > 0:
+            temp=self.head.get_data()
+            self.head=self.head.get_next()
+            return temp
+
+
+    def print_queue(self):
+        current=self.head
+        while current!=None:
+                print current.get_data()
+                current=current.get_next()
+
+
+
+
+
+
+
+
